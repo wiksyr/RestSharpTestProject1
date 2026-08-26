e000e8f5e721c41fd02d48d5cc2cc4f000c9369d
wsyrekIncore <wsyrek@incore.com>
2026-08-26 13:05:38 +0200

    PutCardsValidationTestss

---
+++ RestSharpTestProject1/Arguments/Providers/PutCardValidationArgumentsProvider.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using RestSharpTestProject1.Arguments.Holders;
+using RestSharpTestProject1.Dtos;
+using System.Collections;
+
+namespace RestSharpTestProject1.Arguments.Providers;
+
+public class PutCardValidationArgumentsProvider : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new CardValidationArgumentsHolder
+            {
+                Params = new List<ParameterDto>
+                {
+                    new ParameterDto { Name = "id", Value = "invalid_id", Type = ParameterType.UrlSegment },
+                },
+                StatusCode = System.Net.HttpStatusCode.BadRequest,
+                Message = "invalid id"
+            }
+        };
+        yield return new object[]
+        {
+            new CardValidationArgumentsHolder
+            {
+                Params = new List<ParameterDto>
+                {
+                    new ParameterDto { Name = "id", Value = "6a8c73244a9a844697f31821", Type = ParameterType.UrlSegment }
+                },
+                StatusCode = System.Net.HttpStatusCode.NotFound,
+                Message = "not found"
+            }
+        };
+    }
+}
---
+++ RestSharpTestProject1/Tests/Put/PutCardsValidationTests.cs
@@ -0,0 +1,51 @@
+﻿using RestSharp;
+using RestSharpTestProject1.Arguments.Holders;
+using RestSharpTestProject1.Arguments.Providers;
+using RestSharpTestProject1.Consts;
+using RestSharpTestProject1.Extensions;
+using System.Net;
+
+namespace RestSharpTestProject1.Tests.Put;
+
+public class PutCardsValidationTests : BaseTest
+{
+    [Test]
+    [TestCaseSource(typeof(PutCardValidationArgumentsProvider))]
+    public void PutCardsValidationTest(CardValidationArgumentsHolder args)
+    {
+        var updatedCardName = "Updated Card Name " + DateTime.Now.Ticks;
+        var request = GetRestRequestWithAuthorization(CardsEndpoints.PutCardById)
+            .AddParameter("name", updatedCardName, ParameterType.QueryString);
+        request.AddOrUpdateFromDtos(args.Params);
+
+        var response = _client.Execute(request, Method.Put);
+        var responseContent = response.Content; 
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(args.StatusCode), "Status code mismatch");
+            Assert.That(responseContent, Does.Contain(args.Message), "Error message mismatch");
+        });
+    }
+
+    [Test]
+    [TestCaseSource(typeof(AuthValidationArgumentsProvider))]
+    public void PutCardsValidationAuthTest(AuthValidationArgumentsHolder args)
+    {
+        var updatedCardName = "Updated Card Name " + DateTime.Now.Ticks;
+        var request = GetRestRequestWithoutAuthorization(CardsEndpoints.PutCardById)
+            .AddUrlSegment("id", UrlParams.CardIdToUpdate)
+            .AddParameter("name", updatedCardName, ParameterType.QueryString);
+        request.AddOrUpdateFromDtos(args.Params);
+
+        var response = _client.Execute(request, Method.Put);
+        var responseContent = response.Content;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized), "Status code mismatch");
+            Assert.That(responseContent, Does.Contain(args.Message), "Error message mismatch");
+        });
+    }
+
+}
