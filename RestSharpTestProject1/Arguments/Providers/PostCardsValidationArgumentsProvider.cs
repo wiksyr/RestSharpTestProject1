c510bc409fb3aa8e7bc677b8821cf79e339962b1
wsyrekIncore <wsyrek@incore.com>
2026-08-26 10:44:46 +0200

    PostCardsValidationArgumentsProvider

---
+++ RestSharpTestProject1/Arguments/Providers/PostCardsValidationArgumentsProvider.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using RestSharpTestProject1.Arguments.Holders;
+using RestSharpTestProject1.Consts;
+using RestSharpTestProject1.Dtos;
+using System.Collections;
+
+namespace RestSharpTestProject1.Arguments.Providers;
+
+public class PostCardsValidationArgumentsProvider : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new CardValidationArgumentsHolder
+            {
+                Params = new List<ParameterDto>
+                {
+                    new ParameterDto { Name = "idList", Value = UrlParams.ExistingListId + "1", Type = ParameterType.QueryString },
+                    new ParameterDto { Name = "name", Value = "", Type = ParameterType.QueryString }
+                },
+                StatusCode = System.Net.HttpStatusCode.BadRequest,
+                Message = "invalid value for idList"
+            }
+        };
+
+        yield return new object[]
+        {
+            new CardValidationArgumentsHolder
+            {
+                Params = new List<ParameterDto>
+                {
+                    new ParameterDto { Name = "idList", Value = UrlParams.ExistingListId.Replace("3","1"), Type = ParameterType.QueryString },
+                    new ParameterDto { Name = "name", Value = "", Type = ParameterType.QueryString }
+                },
+                StatusCode = System.Net.HttpStatusCode.NotFound,
+                Message = "could not find"
+            }
+        };
+    }
+}
---
+++ RestSharpTestProject1/Tests/Post/PostCardsValidationTests.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using RestSharpTestProject1.Arguments.Holders;
+using RestSharpTestProject1.Arguments.Providers;
+using RestSharpTestProject1.Consts;
+using RestSharpTestProject1.Extensions;
+
+namespace RestSharpTestProject1.Tests.Post;
+
+public class PostCardsValidationTests : BaseTest
+{
+    [Test]
+    [TestCaseSource(typeof(PostCardsValidationArgumentsProvider))]
+    public void PostCardsValidationTest(CardValidationArgumentsHolder args)
+    {
+        var cardName = "Test Card " + DateTime.Now.Ticks;
+        var request = GetRestRequestWithAuthorization(CardsEndpoints.PostCards);
+        request.AddOrUpdateFromDtos(args.Params);
+
+        var response = _client.Execute(request, Method.Post); 
+        var responseContent = response.Content;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(args.StatusCode), $"Expected status code: {args.StatusCode}, but got: {response.StatusCode}. Response content: {responseContent}");
+            Assert.That(responseContent, Does.Contain(args.Message), $"Expected message: {args.Message}, but got: {responseContent}");
+        };
+    }
+}
